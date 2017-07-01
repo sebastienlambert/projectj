909798bd54ea66184877957ae78103d4d84a3a8a
Sebastien Lambert <sebastienlambert@yahoo.fr>
2017-07-01 21:34:42 +0800

    Use only the web project, will see later if we split projects

--- api/src/main/java/projectj/api/application/v1/PackageConstants.java
+++
@@ -1,8 +0,0 @@
-package projectj.api.application.v1;
-
-
-interface PackageConstants {
-    String VERSION = "v1";
-    String BASE_URL = "/api/" + VERSION;
-
-}
--- api/src/main/java/projectj/api/domain/user/UserCommandGateway.java
+++
@@ -1,9 +0,0 @@
-package projectj.api.domain.user;
-
-
-import projectj.core.domain.user.command.CreateUserCommand;
-
-public interface UserCommandGateway {
-
-    void createUser(CreateUserCommand command);
-}
